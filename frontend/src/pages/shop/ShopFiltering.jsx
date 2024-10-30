988444125ac83e79b3a82a4493c299a202274ba9
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-10-30 20:22:15 +1100

    Adding filtering for color and pricing

--- frontend/src/pages/shop/ShopFiltering.jsx
+++ frontend/src/pages/shop/ShopFiltering.jsx
@@ -5,6 +5,7 @@ const ShopFiltering = ({filters, filtersState, setFiltersState, clearFilters}) =
     <div className='space-y-5 flex-shrink-0'>
         <h3>Filters</h3>
         
+        {/* For category */}
         <div className='flex flex-col space-y-2'>
             <h4 className='font-medium text-lg'>Category</h4>
             <hr/>
@@ -20,6 +21,43 @@ const ShopFiltering = ({filters, filtersState, setFiltersState, clearFilters}) =
                 ))
             }
         </div>
+
+        {/* For colors */}
+        <div className='flex flex-col space-y-2'>
+            <h4 className='font-medium text-lg'>Color</h4>
+            <hr/>
+            {
+                filters.colors.map((color) => (
+                    <label key={color} className='capitalize cursor-pointer'>
+                        <input type='radio' name='color' id='color' value={color}
+                        checked ={filtersState.color === color}
+                        onChange={(e) => setFiltersState({...filtersState, color: e.target.value})}
+                        />
+                        <span className='ml-1'>{color}</span>
+                    </label>
+                ))
+            }
+        </div>
+
+        {/* For Pricing */}
+        <div className='flex flex-col space-y-2'>
+            <h4 className='font-medium text-lg'>Price Range</h4>
+            <hr/>
+            {
+                filters.priceRanges.map((range) => (
+                    <label key={range.label} className='capitalize cursor-pointer'>
+                        <input type='radio' name='priceRange' id='priceRange' 
+                        value={`${range.min}-${range.max}`}
+                        checked ={filtersState.priceRange === `${range.min}-${range.max}`}
+                        onChange={(e) => setFiltersState({...filtersState, priceRange: e.target.value})}
+                        />
+                        <span className='ml-1'>{range.label}</span>
+                    </label>
+                ))
+            }
+        </div>
+        
+
     </div>
   )
 }
