b1a97307fc8845af489f466325ebb79cd8015105
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-10-31 17:57:45 +1100

    Added sample product image, descriptions and rating for frontend

--- frontend/src/pages/shop/productDetails/SingleProduct.jsx
+++ frontend/src/pages/shop/productDetails/SingleProduct.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import RatingStars from '../../../components/RatingStars';
 
 const SingleProduct = () => {
     const {id} = useParams();
-    console.log(id)
+    //console.log(id)
   return (
     <>
     <section className='section__container bg-primary-light'>
@@ -16,6 +17,36 @@ const SingleProduct = () => {
             <span className='hover:text-primary'>product name</span>
         </div>
     </section>
+    
+    <section className='section__container mt-8'>
+        <div className='flex flex-col items-center md:flex-row gap-8'>
+            {/* product image */}
+            <div className='md:w-1/2 w-full'>
+                <img src='https://images.unsplash.com/photo-1469334031218-e382a71b716b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+                alt='Product Image' className='rounded-md w-full h-auto'/>
+            </div>
+
+            <div className='md:w-1/2 w-full'>
+                <h3 className='text-2xl font-semibold mb-4'>Product Name</h3>
+                <p className='text-xl text-primary mb-4'>$100 <s>130</s></p>
+                <p className='text-gray-700 mb 4'>This is product description</p>
+
+                {/* additional product info */}
+                <div>
+                    <p><strong>Category:</strong> accessories</p>
+                    <p><strong>Color: </strong>Red</p>
+                    <div className='flex gap-1 items-center'>
+                        <strong>Rating: </strong>
+                        <RatingStars rating={"4"}/>
+                    </div>
+                </div>
+
+                {/* button */}
+                <button className='mt-6 px-6 py-3 bg-primary text-white rounded-md'>Add to Cart</button>
+
+            </div>
+        </div>
+    </section> 
     </>
   )
 }
