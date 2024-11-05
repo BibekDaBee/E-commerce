bc76a39b28056b0550566064fdcdfa8fd79c4c2b
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-05 11:20:22 +1100

    Install packages: -CORS(Cross-Origin Resource Sharing) -cookie-parser -body-parser -setting up middleware setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config(); // Load environment variables from .env file
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+//middleware setup
+app.use(express.json({limit:"25mb"}));
+app.use(express.urlencoded({limit:"25mb"}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials: true
+}))
 
 
 main().then(() => console.log("Mongobd is successfully connected")).catch(err => console.log(err));
