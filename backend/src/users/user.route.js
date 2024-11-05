e785db302cbb22e17f0e0aa13368f1d1f106b05f
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-05 18:55:03 +1100

    -Installing jsonwebtoken package for managing token -Creating "generateTokens.js" -Only passing user ID, email, username, role, profile Image, bio and profession as a response for security purpose

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('./user.model');
+const generateToken = require('../middleware/generateToken');
 const router = express.Router();
 
 // Register endpoint
@@ -31,7 +32,24 @@ router.post('/login', async (req, res) => {
         return res.status(401).send({message:'Password not matched'})
     }
 
-    res.status(200).send({message:"Logged in Successfully", user})
+    const token = await generateToken(user.id);
+    // console.log("token",token)
+
+    res.cookie('token', token,{
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
+    })
+
+    res.status(200).send({message:"Logged in Successfully", token, user:{
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        role: user.role,
+        profileImage: user.profileImage,
+        bio: user.bio,
+        profession: user.profession
+    }})
     } catch (error) {
         console.log("Error logging in user", error);
         res.status(500).send({message:"Error logging in user"})
