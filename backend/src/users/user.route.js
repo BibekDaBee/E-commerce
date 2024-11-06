87bee6a96b3a20c421653554342271800214c56a
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-06 20:15:37 +1100

    Added Update User role endpoint by using "/users/:id" "Put" endpoint to update the role of the user in the database

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -94,4 +94,21 @@ router.get('/users', async (req, res) =>{
     }
 })
 
+// Update User role
+router.put('/users/:id', async(req, res) =>{
+    try {
+        const {id} =req.params;
+        const {role} = req.body;
+        const user = await User.findByIdAndUpdate(id, {role}, {new: true});
+        if(!user){
+            return res.status(404).send({message:'User not found'})
+        }
+        res.status(200).send({message:'User role updated successfully', user})
+        
+    } catch (error) {
+        console.log("Error updating user role", error);
+        res.status(500).send({message:"Error updating user role"})
+    }
+})
+
 module.exports = router;
