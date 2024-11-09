24cf7a6daec7debb6a27fb809c6420976368f970
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-09 16:10:49 +1100

    Updating "product.route.js" to fetch data of single product

--- backend/src/products/products.route.js
+++ backend/src/products/products.route.js
@@ -62,5 +62,21 @@ router.get("/", async (req, res) =>{
     }
 })
 
+// Get Single Products
+router.get("/:id", async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const product = await Products.findById(productId).populate("author", "email username");
+        if(!product){
+            return res.status(404).send({message: "Product not found"});
+        }
+        const reviews = await Reviews.find({productId}).populate("userId", "username email");
+        res.status(200).send({product, reviews});
+        
+    } catch (error) {
+        console.error("Error fetching Product", error);
+        res.status(500).send({message: "Failed to fetch the Product"});
+    }
+})
 
 module.exports = router;
