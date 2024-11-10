4ce99caca1310f23ae6ddd1ca32ede7e3fb60a94
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-10 18:41:42 +1100

    feat(productsApi): Add RTK Query endpoints for managing product

---
+++ frontend/src/redux/features/products/productsAPI.js
@@ -0,0 +1,83 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getBaseURL } from "../../../utils/baseURL";
+
+const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${getBaseURL()}/api/products`,
+    credentials: "include",
+  }),
+  tagTypes: ["Products"],
+  endpoints: (builder) => ({
+    fetchAllProducts: builder.query({
+      query: ({
+        category,
+        color,
+        minPrice,
+        maxPrice,
+        page = 1,
+        limit = 10,
+      }) => {
+        const queryParams = new URLSearchParams({
+          category: category || "",
+          color: color || "",
+          minPrice: minPrice || 0,
+          maxPrice: maxPrice || "",
+          page: page.toString(),
+          limit: limit.toString(),
+        }).toString();
+        return `/?${queryParams}`;
+      },
+      providesTags: ["Products"],
+    }),
+
+    //Get Single Product (fetch by ID)
+    fetchProductById: (builder) => ({
+      query: (id) => `/${id}`,
+      providesTags: (result, error, id) => [{ type: "Products", id }],
+    }),
+
+    // Adding a product route
+    AddProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: "/create-product",
+        method: "POST",
+        body: newProduct,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Products"],
+    }),
+
+    //Getting related Product
+    fetchRelatedProducts: builder.query({
+      query: (id) => `/related/${id}`,
+    }),
+
+    //Updating a product
+    updateProduct: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `/update-product/${id}`,
+        method: "PATCH",
+        body: rest,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Products"],
+    }),
+
+    // Delete Product
+    deleteProduct: builder.mutation({
+      query: ({ id }) => ({
+        url: `/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: (result, error, id) => [{ type: "Products", id }],
+    }),
+  }),
+});
+
+
+export const {useFetchAllProductsQuery, useFetchProductByIdQuery, useAddProductMutation, 
+    useFetchRelatedProductsQuery, useUpdateProductMutation, useDeleteProductMutation} = productsApi;
+
+export default productsApi;
