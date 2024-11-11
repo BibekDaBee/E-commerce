8d38721361b62ddcc7975aba61abf8fffbafd8f6
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-11 20:52:47 +1100

    completing reviews route in backend

--- backend/src/reviews/reviews.router.js
+++ backend/src/reviews/reviews.router.js
@@ -55,4 +55,35 @@ router.post("/post-review", async (req, res) => {
   }
 });
 
+// total review count
+router.get("/total-reviews", async (req, res) => {
+  try {
+    const totalReviews = await Reviews.countDocuments({});
+    res.status(200).send({totalReviews});
+
+
+  } catch (error) {
+    console.error("Error getting total review", error);
+    res.status(500).send({ message: "Failed to get review count" });
+  }
+})
+
+// get reviews by user Id
+ router.get("/:userId", async (req, res) => {
+  const {userId} = req.params;
+  if(!userId){
+    return res.status(400).send({ message: "User Id is required" });
+  }
+  try {
+    const reviews = await Reviews.find({userId: userId}).sort({createAt: -1});
+    if(reviews.length === 0){
+      return res.status(404).send({ message: "No reviews found" });
+    }
+    res.status(200).send(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews by user", error);
+    res.status(500).send({ message: "Failed to fetch review by user" });
+  }
+ })
+
 module.exports = router;
