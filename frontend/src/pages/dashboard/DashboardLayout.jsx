a26830a8c794b4b169c34c26e546073781f8e677
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-16 02:19:13 +1100

    Creating a dashboard layout. Showing the respective layout for admin and user and navigating to login if there is no user signed in

--- frontend/src/pages/dashboard/DashboardLayout.jsx
+++ frontend/src/pages/dashboard/DashboardLayout.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
+  const {user} = useSelector((state) => state.auth)
+  if(!user){
+    return <Navigate to="/login" replace/>
+  }
+
+  const renderDashboard = () => {
+    switch (user?.role) {
+      case 'admin':
+        return <div>Admin Dashboard</div>;
+      case 'user':
+        return <div>User Dashboard</div>
+      default:
+        return <Navigate to='/login' replace/>;
+    }
+  }
+
   return (
-    <div>
+    <div className='container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start'>
+      <header className='lg:w-1/5 sm:2/5 w-full border'>
+      {renderDashboard()}
+      </header>
+      <main className='p-8 bg-white w-full border mt-5'>
         <Outlet/>
+      </main>
     </div>
   )
 }
