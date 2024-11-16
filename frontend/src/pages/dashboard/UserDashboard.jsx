544d61b82cc8c9bfe4eada3a4a1d637f38cf0afe
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-11-16 11:52:07 +1100

    Setting up User Dashboard and connecting to different user dashboard components, Adding Logout Button on the Dashboard

---
+++ frontend/src/pages/dashboard/UserDashboard.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { useLogoutUserMutation } from '../../redux/features/auth/authApi'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../redux/features/auth/authSlice'
+const navItems = [
+    {path: '/dashboard', label: 'Dashboard'},
+    {path: '/dashboard/orders', label: 'Orders'},
+    {path: '/dashboard/payments', label: 'Payments'},
+    {path: '/dashboard/profile', label: 'Profile'},
+    {path: '/dashboard/reviews', label: 'Reviews'},
+]
+
+const UserDashboard = () => {
+    const [logoutUser] = useLogoutUserMutation();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = async() => {
+        try {
+            await logoutUser().unwrap();
+            alert('logout successful');
+            dispatch(logout());
+            navigate('/')
+        } catch (error) {
+            console.error("Failed to logout", error)
+        }
+    }
+  return (
+    <div className='space-y-5 bg-white p-8 md:h-screen flex flex-col justify-between'>
+        <div>
+            <div className='nav__logo'>
+                <Link>Ecommerce<span>.</span></Link>
+                <p className='text-sx italic'>User Dashboard</p>
+            </div>
+            <hr className='mt-t'/>
+            <ul className='space-y-5 pt-10'>
+                {
+                    navItems.map((item) => (
+                        <li key={item.path}>
+                            <NavLink 
+                            className={({isActive}) => isActive ? "text-blue-500 font-bold" : "text-black"} end
+                            to={item.path}
+                            >
+                                {item.label}
+                            </NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+        
+        {/* Logout button */}
+        <div className='mb-3'>
+            <hr className='mb-3'/>
+            <button 
+            onClick={handleLogout}
+            className='text-white bg-primary font-medium px-5 py-1 rounded-sm'>Logout</button>
+        </div>
+    </div>
+  )
+}
+
+export default UserDashboard
