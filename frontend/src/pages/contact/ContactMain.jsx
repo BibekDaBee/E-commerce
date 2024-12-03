212cd149af16d49034a7bbe5932b039229feb964
Bibek Dallakoti <140939649+BibekDaBee@users.noreply.github.com>
2024-12-03 18:26:13 +1100

    Updating front end and install web3forms npm package

--- frontend/src/pages/contact/ContactMain.jsx
+++ frontend/src/pages/contact/ContactMain.jsx
@@ -1,65 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import './contact.css'
-
+import React, { useEffect, useState } from "react";
+import "./contact.css";
+import Swal from "sweetalert2";
 
 const ContactMain = () => {
-    const [result, setResult] = useState('');
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const key = import.meta.env.VITE_WEB3FORM_SECRET_KEY;
-    // console.log(key);
+  const key = import.meta.env.VITE_WEB3FORM_SECRET_KEY;
+  // console.log(key);
+  if (!key) {
+    console.error(
+      "VITE_WEB3FORM_SECRET_KEY is not defined in the environment variables."
+    );
+  }
 
-    // Email submission function using web3form
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        setResult("Sending....");
-        const formData = new FormData(event.target);
-    
-        formData.append("access_key", key);
+  // Email submission function using web3form
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-        try {
-            const response = await fetch("https://api.web3forms.com/submit", {
-                method: "POST",
-                body: formData
-              });
-          
-              const data = await response.json();
-          
-              if (data.success) {
-                setResult("Form Submitted Successfully");
-                event.target.reset();
-              } else {
-                console.log("Error", data);
-                setResult(data.message);
-              }
-        } catch (error) {
-            console.error("Submission error:", error);
-            setResult("An error occurred while submitting the form.");
-        }
-      };
+    formData.append("access_key", key);
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+        Swal.fire({
+          title: "Success!",
+          text: "Message send Successfully!",
+          icon: "success",
+        });
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      }
+    } catch (error) {
+      console.error("Submission error:", error);
+      setResult("An error occurred while submitting the form.");
+    }
+  };
   return (
-    <section className='contact'>
-        <form onSubmit={onSubmit}>
-            <h2>Contact form</h2>
-            <div className='input-box'>
-                <label>Full Name</label>
-                <input type='text' className='field' placeholder='Enter your full name' name='name' required/>
-            </div>
-            <div className='input-box'>
-                <label>Email Address</label>
-                <input type='email' className='field' placeholder='Enter your email address' name='email' required/>
-            </div>
-            <div className='input-box'>
-                <label>Message</label>
-                <input type='text' className='field mess' placeholder='Enter your message' name='message' required/>
-            </div>
-            <button type='submit'>Send Message</button>
-            {result && <p>{result}</p>}
-        </form>
+    <section className="contact">
+      <form onSubmit={onSubmit}>
+        <h2>Contact form</h2>
+        <div className="input-box">
+          <label>Full Name</label>
+          <input
+            type="text"
+            className="field"
+            placeholder="Enter your full name"
+            name="name"
+            required
+          />
+        </div>
+        <div className="input-box">
+          <label>Email Address</label>
+          <input
+            type="email"
+            className="field"
+            placeholder="Enter your email address"
+            name="email"
+            required
+          />
+        </div>
+        <div className="input-box">
+          <label>Message</label>
+          <textarea
+            id="message"
+            className="field mess"
+            placeholder="Enter your message"
+            name="message"
+            required
+          />
+        </div>
+        <button type="submit">Send Message</button>
+        {result && <p>{result}</p>}
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default ContactMain
+export default ContactMain;
